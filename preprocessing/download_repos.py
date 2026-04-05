87c7e5b80224da59cedd734ba391c8f66bcd1f65
Francesco Tosoni <francitosoni@gmail.com>
2026-04-05 21:52:47 +0200

    Finalize project: add license headers, metadata, and core application files

--- preprocessing/download_repos.py
+++ preprocessing/download_repos.py
@@ -1,3 +1,19 @@
+# This file is part of MediaWiki Code2Code Search
+# <https://github.com/ftosoni/mediawiki-code2code-search>.
+# Copyright (c) 2026 Francesco Tosoni.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import json
 import os
 import subprocess
@@ -142,4 +158,4 @@ def download_repositories(json_file, target_dir="mediawiki_repos"):
         print(f"List of failed clones saved to failed_clones.json")
 
 if __name__ == "__main__":
-    download_repositories("repos_list.json")
+    download_repositories("repos_list.json")
